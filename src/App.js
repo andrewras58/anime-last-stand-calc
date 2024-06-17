0bf233a34597991e2cb9d88a8bedde5693533db6
andrewras58 <andrewras58@gmail.com>
2024-06-17 14:50:03 -0700

    got rid of the router for now so gh-pages stops breaking

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import './scss/App.scss';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import Navbar from './Navbar';
 import TechniqueReroll from './TechniqueReroll';
-import StatReroll from './StatReroll';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <div className="App">
-        {/* <Navbar /> */}
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<TechniqueReroll />}></Route>
-            <Route path="/stats" element={<StatReroll />}></Route>
-          </Routes>
-        </div>
-      </div>
-    </Router>
+    <div className="App">
+      <TechniqueReroll />
+    </div>
   );
 }
 
